c47b65a3883ced204d7d8ea6f3cb1d52f8f553d3
Kipnation <126730275+Kipnation@users.noreply.github.com>
2023-03-06 13:04:17 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t2Xrlnp8ZyQkpHWcBvJn2NbAZ8vR77uvrLQbzaR-DdA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDfHg1OVPHCO3RVIo-VRELLn8PH0FZuiAo';
